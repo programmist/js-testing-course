c3cc4424f588e663e1ef2cd66418f47d337998f4
Tony Childs <tony.childs@gmail.com>
2024-03-03 15:34:32 -0600

    add test for fizzbuzz

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, expect, test, it } from "vitest";
-import { max } from "../src/intro";
+import { fizzBuzz, max } from "../src/intro";
 
 describe("max", () => {
   test("First agrument shoud be greater", () => {
@@ -12,5 +12,23 @@ describe("max", () => {
 
   test("Agruments should be equal", () => {
     expect(max(2, 2)).toBe(2);
+  });
+});
+
+describe("fizzbuzz", () => {
+  test("Should return 'Fizz' if arg is be divisible by 3 only", () => {
+    expect(fizzBuzz(6)).toBe("Fizz");
+  });
+
+  test("Should return 'Buzz' if arg is be divisible by 5 only", () => {
+    expect(fizzBuzz(10)).toBe("Buzz");
+  });
+
+  test("Should return 'FizzBuzz' if arg is be divisible by 3 and 5", () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz");
+  });
+
+  test("Should return string value of arg when arg is divisible by neither 3 nor 5", () => {
+    expect(fizzBuzz(11)).toBe("11");
   });
 });
