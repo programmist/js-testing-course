c2c1d029f76da67cf192ff96e123f8ddb367f65a
Tony Childs <tony.childs@gmail.com>
2024-03-04 18:07:00 -0600

    Add factorial function and tests

--- src/intro.js
+++ src/intro.js
@@ -10,3 +10,14 @@ export function fizzBuzz(n) {
   if (n % 5 === 0) return "Buzz";
   return n.toString();
 }
+
+export function calculateAvg(arr) {
+  if (arr.length === 0) return NaN;
+  return arr.reduce((acc, val) => acc + val, 0) / arr.length;
+}
+
+export function factorial(n) {
+  if (n < 0) return undefined;
+  if (n < 2) return 1;
+  return n * factorial(n - 1);
+}
--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,16 +1,16 @@
-import { describe, expect, test, it } from "vitest";
-import { fizzBuzz, max } from "../src/intro";
+import { describe, expect, test } from "vitest";
+import { calculateAvg, factorial, fizzBuzz, max } from "../src/intro";
 
 describe("max", () => {
-  test("First agrument shoud be greater", () => {
+  test("First argument should be greater", () => {
     expect(max(2, 1)).toEqual(2);
   });
 
-  test("Second agrument shoud be greater", () => {
+  test("Second argument should be greater", () => {
     expect(max(2, 3)).toBe(3);
   });
 
-  test("Agruments should be equal", () => {
+  test("Arguments should be equal", () => {
     expect(max(2, 2)).toBe(2);
   });
 });
@@ -30,5 +30,45 @@ describe("fizzbuzz", () => {
 
   test("Should return string value of arg when arg is divisible by neither 3 nor 5", () => {
     expect(fizzBuzz(11)).toBe("11");
+  });
+});
+
+describe("arrayAvg", () => {
+  test("Should return the average when all values are numbers", () => {
+    expect(calculateAvg([1, 2, 3, 4, 5])).toBe(3);
+  });
+
+  test("Should return the average when array contains one number", () => {
+    expect(calculateAvg([1])).toBe(1);
+  });
+
+  test("Should return NaN when the array is empty", () => {
+    expect(calculateAvg([])).toBe(NaN);
+  });
+});
+
+describe("factorial", () => {
+  test("Factorial of a negative number should return undefined", () => {
+    expect(factorial(-3)).toBeUndefined();
+  });
+
+  test("Zero factorial should return 0", () => {
+    expect(factorial(0)).toBe(1);
+  });
+
+  test("One factorial should return 0", () => {
+    expect(factorial(1)).toBe(1);
+  });
+
+  test("Two factorial should return 2", () => {
+    expect(factorial(2)).toBe(2);
+  });
+
+  test("Three factorial should return 6", () => {
+    expect(factorial(3)).toBe(6);
+  });
+
+  test("Four factorial should return 24", () => {
+    expect(factorial(4)).toBe(24);
   });
 });
