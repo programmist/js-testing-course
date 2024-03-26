c60a40fb0b2faa5add86ab455b72d262bf819775
Tony Childs <tony.childs@gmail.com>
2024-03-25 19:59:13 -0500

    configure typescript-eslint integration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,13 @@
+// TODO: Convert to Eslint 9.0 and update to eslint.config.js
 module.exports = {
   env: {
     browser: true,
     es2021: true,
   },
-  extends: "standard",
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
   overrides: [
     {
       env: {
